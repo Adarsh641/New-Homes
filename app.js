f95cfd8075317a6e1e8ba98d0f006520541ea739
Adarsh goutam <adarshgoutam@Adarshs-MacBook-Air-5.local>
2026-08-01 01:09:22 +0530

    Add Docker configuration for deployment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  const express = require('express');
  const app=express();
  const mongoose= require('mongoose');
- const MONGO_URL="mongodb://127.0.0.1:27017/newhomes";
+ const MONGO_URL = process.env.MONGO_URL;
  const path= require('path');
  const methodOverride= require('method-override');
  const ExpressError=require('./utils/ExpressError');
